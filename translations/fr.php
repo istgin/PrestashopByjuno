e1dca363d94f3b6d449aad108189313b8fae7058
Igor Sutugin <igor.sutugin@ugcs.com>
2018-07-11 21:58:42 +0300

    translations

--- translations/fr.php
+++ translations/fr.php
@@ -1,46 +1,46 @@
-<?php
+﻿<?php
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{byjuno}prestashop>payment_1335094cae0deb52ff6efc9b8840e8de'] = 'Pay by byjuno Rechnung';
+$_MODULE['<{byjuno}prestashop>payment_1335094cae0deb52ff6efc9b8840e8de'] = 'Payer par facture Byjuno';
 $_MODULE['<{byjuno}prestashop>payment_0388a4ccc8d2065cdc94da8fba58a30a'] = 'Facture Byjuno';
-$_MODULE['<{byjuno}prestashop>payment_4a08d53adc4052023681df0c2a89b073'] = 'Pay by byjuno installment';
-$_MODULE['<{byjuno}prestashop>payment_ee946970cb5f21c257e87b07e2122e06'] = 'Byjuno Paiement échelonné';
+$_MODULE['<{byjuno}prestashop>payment_4a08d53adc4052023681df0c2a89b073'] = 'Paiement échelonné Byjuno';
+$_MODULE['<{byjuno}prestashop>payment_ee946970cb5f21c257e87b07e2122e06'] = 'Paiement échelonné Byjuno';
 
-$_MODULE['<{byjuno}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Your order on %s is complete.';
-$_MODULE['<{byjuno}prestashop>payment_return_b2f40690858b404ed10e62bdf422c704'] = 'Amount';
-$_MODULE['<{byjuno}prestashop>payment_return_0f09dc266f1e577e5ac4dc3c5cb84138'] = 'Order reference %s';
-$_MODULE['<{byjuno}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'We noticed a problem with your order. If you think this is an error, feel free to contact our';
-$_MODULE['<{byjuno}prestashop>payment_return_66fcf4c223bbf4c7c886d4784e1f62e4'] = 'expert customer support team';
+$_MODULE['<{byjuno}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Votre commande chez %s est complète.';
+$_MODULE['<{byjuno}prestashop>payment_return_b2f40690858b404ed10e62bdf422c704'] = 'Montant';
+$_MODULE['<{byjuno}prestashop>payment_return_0f09dc266f1e577e5ac4dc3c5cb84138'] = 'Référence commance %s';
+$_MODULE['<{byjuno}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'Nous avons remarqué un problème dans votre commande. Si vous pensez qu\'il s\'agit d\'une erreur, veuillez contacter notre';
+$_MODULE['<{byjuno}prestashop>payment_return_66fcf4c223bbf4c7c886d4784e1f62e4'] = 'service clients';
 
-$_MODULE['<{byjuno}prestashop>payment_error_639165db94bc68d5db4a0f4e11802f11'] = 'Byjuno payments';
-$_MODULE['<{byjuno}prestashop>payment_error_1e97d97a923eaddd810e056c828e99ea'] = 'Payment error';
-$_MODULE['<{byjuno}prestashop>payment_error_c74e2d6724e2edc933a7bded86301b08'] = 'Order processing error. Please try again.';
-$_MODULE['<{byjuno}prestashop>payment_error_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
+$_MODULE['<{byjuno}prestashop>payment_error_639165db94bc68d5db4a0f4e11802f11'] = 'Paiement Byjuno';
+$_MODULE['<{byjuno}prestashop>payment_error_1e97d97a923eaddd810e056c828e99ea'] = 'Erreur de paiement';
+$_MODULE['<{byjuno}prestashop>payment_error_c74e2d6724e2edc933a7bded86301b08'] = 'Erreur de traitement de votre commande. Veuillez réessayer.';
+$_MODULE['<{byjuno}prestashop>payment_error_569fd05bdafa1712c4f6be5b153b8418'] = 'Autres méthodes de paiement';
 
-$_MODULE['<{byjuno}prestashop>payment_execution_e7a40b36fd632b62f6f00d89030832b7'] = 'Byjuno Facture avec option de paiement partiel';
-$_MODULE['<{byjuno}prestashop>payment_execution_b8405c16fab03dd6ba7f50aa1805a412'] = 'Byjuno facture unique';
-$_MODULE['<{byjuno}prestashop>payment_execution_dbd37025b503190124596363e32f3c2e'] = '3 tranches';
-$_MODULE['<{byjuno}prestashop>payment_execution_ec3938629be3a08e356cca49ac068a29'] = '10 tranches';
-$_MODULE['<{byjuno}prestashop>payment_execution_9ed230c74799a3db4bdc4294b42080bd'] = '12 tranches';
-$_MODULE['<{byjuno}prestashop>payment_execution_ef5010d8911ba4b1a4d2665c22a8e51c'] = '24 tranches';
-$_MODULE['<{byjuno}prestashop>payment_execution_5d92fdfa267376f9c89c1a26f1d2e1c4'] = '4 tranches dans 12 mois';
+$_MODULE['<{byjuno}prestashop>payment_execution_e7a40b36fd632b62f6f00d89030832b7'] = 'Facture Byjuno avec option de paiement partiel';
+$_MODULE['<{byjuno}prestashop>payment_execution_b8405c16fab03dd6ba7f50aa1805a412'] = 'Facture unique Byjuno';
+$_MODULE['<{byjuno}prestashop>payment_execution_dbd37025b503190124596363e32f3c2e'] = '3 échéances';
+$_MODULE['<{byjuno}prestashop>payment_execution_ec3938629be3a08e356cca49ac068a29'] = '10 échéances';
+$_MODULE['<{byjuno}prestashop>payment_execution_9ed230c74799a3db4bdc4294b42080bd'] = '12 échéances';
+$_MODULE['<{byjuno}prestashop>payment_execution_ef5010d8911ba4b1a4d2665c22a8e51c'] = '24 échéances';
+$_MODULE['<{byjuno}prestashop>payment_execution_5d92fdfa267376f9c89c1a26f1d2e1c4'] = '4 échéances en 12 mois';
 
 $_MODULE['<{byjuno}prestashop>payment_execution_06aac5c4279d0d93b5527804b88827e1'] = 'Facture Byjuno';
-$_MODULE['<{byjuno}prestashop>payment_execution_b7d009d8f8d9c3d5e07692546dcf3806'] = 'Byjuno Paiement échelonné';
+$_MODULE['<{byjuno}prestashop>payment_execution_b7d009d8f8d9c3d5e07692546dcf3806'] = 'Paiement échelonné Byjuno';
 
-$_MODULE['<{byjuno}prestashop>payment_execution_962c29e9d818d87968b7460bf9a6472b'] = 'Select payment';
-$_MODULE['<{byjuno}prestashop>payment_execution_879f6b8877752685a966564d072f498f'] = 'Your shopping cart is empty.';
-$_MODULE['<{byjuno}prestashop>payment_execution_de373d72d209506f178c46735ecbf447'] = 'Select payment plan';
-$_MODULE['<{byjuno}prestashop>payment_execution_eaf1849876a6548ecc5b804d7da44b27'] = 'Select invoice delivery method';
-$_MODULE['<{byjuno}prestashop>payment_execution_dfc27ef27b631b0e7114573a439c3180'] = 'Expédition facture via e-mail (sans frais)';
-$_MODULE['<{byjuno}prestashop>payment_execution_3cd02748d4e2c00844a284b71a479ba1'] = 'Expédition facture sous forme papier (au prix de CHF 3.50)';
-$_MODULE['<{byjuno}prestashop>payment_execution_019ec3132cdf8ee0f2e2a75cf5d3e459'] = 'Gender';
-$_MODULE['<{byjuno}prestashop>payment_execution_63889cfb9d3cbe05d1bd2be5cc9953fd'] = 'Male';
-$_MODULE['<{byjuno}prestashop>payment_execution_b719ce180ec7bd9641fece2f920f4817'] = 'Female';
-$_MODULE['<{byjuno}prestashop>payment_execution_10803b83a68db8f7e7a33e3b41e184d0'] = 'Date of Birth';
-$_MODULE['<{byjuno}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
-$_MODULE['<{byjuno}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'I confirm my order';
+$_MODULE['<{byjuno}prestashop>payment_execution_962c29e9d818d87968b7460bf9a6472b'] = 'Selectionnez la méthode de paiement';
+$_MODULE['<{byjuno}prestashop>payment_execution_879f6b8877752685a966564d072f498f'] = 'Il n\'y a aucun article dans votre panier.';
+$_MODULE['<{byjuno}prestashop>payment_execution_de373d72d209506f178c46735ecbf447'] = 'Sélectionnez le plan de paiement';
+$_MODULE['<{byjuno}prestashop>payment_execution_eaf1849876a6548ecc5b804d7da44b27'] = 'Sélectionnez la méthode de livraison de vos factures';
+$_MODULE['<{byjuno}prestashop>payment_execution_dfc27ef27b631b0e7114573a439c3180'] = 'Envoi des factures par courrier électronique à (gratuit)';
+$_MODULE['<{byjuno}prestashop>payment_execution_3cd02748d4e2c00844a284b71a479ba1'] = 'Envoi des facture par courrier à (Frais de CHF 3.50 par lettre)';
+$_MODULE['<{byjuno}prestashop>payment_execution_019ec3132cdf8ee0f2e2a75cf5d3e459'] = 'Sexe';
+$_MODULE['<{byjuno}prestashop>payment_execution_63889cfb9d3cbe05d1bd2be5cc9953fd'] = 'Masculin';
+$_MODULE['<{byjuno}prestashop>payment_execution_b719ce180ec7bd9641fece2f920f4817'] = 'Féminin';
+$_MODULE['<{byjuno}prestashop>payment_execution_10803b83a68db8f7e7a33e3b41e184d0'] = 'Date de naîssance';
+$_MODULE['<{byjuno}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Autres méthodes de paiement';
+$_MODULE['<{byjuno}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'Je confirme ma commande';
 
 
 return $_MODULE;
--- translations/it.php
+++ translations/it.php
@@ -1,22 +1,22 @@
-<?php
+﻿<?php
 global $_MODULE;
 $_MODULE = array();
 
-$_MODULE['<{byjuno}prestashop>payment_1335094cae0deb52ff6efc9b8840e8de'] = 'Pay by byjuno invoice';
+$_MODULE['<{byjuno}prestashop>payment_1335094cae0deb52ff6efc9b8840e8de'] = 'Pagare la fattura con Byjuno';
 $_MODULE['<{byjuno}prestashop>payment_0388a4ccc8d2065cdc94da8fba58a30a'] = 'Fattura Byjuno';
-$_MODULE['<{byjuno}prestashop>payment_4a08d53adc4052023681df0c2a89b073'] = 'Pay by byjuno installment';
+$_MODULE['<{byjuno}prestashop>payment_4a08d53adc4052023681df0c2a89b073'] = 'Pagare a rate con Byjuno';
 $_MODULE['<{byjuno}prestashop>payment_ee946970cb5f21c257e87b07e2122e06'] = 'Byjuno Pagamento rateale';
 
-$_MODULE['<{byjuno}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Your order on %s is complete.';
-$_MODULE['<{byjuno}prestashop>payment_return_b2f40690858b404ed10e62bdf422c704'] = 'Amount';
-$_MODULE['<{byjuno}prestashop>payment_return_0f09dc266f1e577e5ac4dc3c5cb84138'] = 'Order reference %s';
-$_MODULE['<{byjuno}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'We noticed a problem with your order. If you think this is an error, feel free to contact our';
-$_MODULE['<{byjuno}prestashop>payment_return_66fcf4c223bbf4c7c886d4784e1f62e4'] = 'expert customer support team';
+$_MODULE['<{byjuno}prestashop>payment_return_88526efe38fd18179a127024aba8c1d7'] = 'Il tuo ordine da %s è completo.';
+$_MODULE['<{byjuno}prestashop>payment_return_b2f40690858b404ed10e62bdf422c704'] = 'somma';
+$_MODULE['<{byjuno}prestashop>payment_return_0f09dc266f1e577e5ac4dc3c5cb84138'] = 'riferimento dell\'ordine %s';
+$_MODULE['<{byjuno}prestashop>payment_return_d15feee53d81ea16269e54d4784fa123'] = 'C\'è un problema con l\'ordine. La preghiamo di contattare il nostro';
+$_MODULE['<{byjuno}prestashop>payment_return_66fcf4c223bbf4c7c886d4784e1f62e4'] = 'servizio cliente';
 
-$_MODULE['<{byjuno}prestashop>payment_error_639165db94bc68d5db4a0f4e11802f11'] = 'Byjuno payments';
-$_MODULE['<{byjuno}prestashop>payment_error_1e97d97a923eaddd810e056c828e99ea'] = 'Payment error';
-$_MODULE['<{byjuno}prestashop>payment_error_c74e2d6724e2edc933a7bded86301b08'] = 'Order processing error. Please try again.';
-$_MODULE['<{byjuno}prestashop>payment_error_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
+$_MODULE['<{byjuno}prestashop>payment_error_639165db94bc68d5db4a0f4e11802f11'] = 'Pagare con Byjuno';
+$_MODULE['<{byjuno}prestashop>payment_error_1e97d97a923eaddd810e056c828e99ea'] = 'Errore di pagamento';
+$_MODULE['<{byjuno}prestashop>payment_error_c74e2d6724e2edc933a7bded86301b08'] = 'Il fornitore di pagamenti ha rifiutato il pagamento. La preghiamo di provare un altro metodo di pagamento.';
+$_MODULE['<{byjuno}prestashop>payment_error_569fd05bdafa1712c4f6be5b153b8418'] = 'altri metodi di pagamenti';
 
 $_MODULE['<{byjuno}prestashop>payment_execution_e7a40b36fd632b62f6f00d89030832b7'] = 'Byjuno fattura con opzione di pagamento parziale';
 $_MODULE['<{byjuno}prestashop>payment_execution_b8405c16fab03dd6ba7f50aa1805a412'] = 'Byjuno fattura singola';
@@ -29,18 +29,18 @@ $_MODULE['<{byjuno}prestashop>payment_execution_5d92fdfa267376f9c89c1a26f1d2e1c4
 $_MODULE['<{byjuno}prestashop>payment_execution_06aac5c4279d0d93b5527804b88827e1'] = 'Fattura Byjuno';
 $_MODULE['<{byjuno}prestashop>payment_execution_b7d009d8f8d9c3d5e07692546dcf3806'] = 'Byjuno Pagamento rateale';
 
-$_MODULE['<{byjuno}prestashop>payment_execution_962c29e9d818d87968b7460bf9a6472b'] = 'Select payment';
-$_MODULE['<{byjuno}prestashop>payment_execution_879f6b8877752685a966564d072f498f'] = 'Your shopping cart is empty.';
-$_MODULE['<{byjuno}prestashop>payment_execution_de373d72d209506f178c46735ecbf447'] = 'Select payment plan';
-$_MODULE['<{byjuno}prestashop>payment_execution_eaf1849876a6548ecc5b804d7da44b27'] = 'Select invoice delivery method';
-$_MODULE['<{byjuno}prestashop>payment_execution_dfc27ef27b631b0e7114573a439c3180'] = 'Invio fattura tramite e-mail (senza spese aggiuntive)';
-$_MODULE['<{byjuno}prestashop>payment_execution_3cd02748d4e2c00844a284b71a479ba1'] = 'Invio fattura cartacea tramite posta (spese aggiuntive di CHF 3.50)';
-$_MODULE['<{byjuno}prestashop>payment_execution_019ec3132cdf8ee0f2e2a75cf5d3e459'] = 'Gender';
-$_MODULE['<{byjuno}prestashop>payment_execution_63889cfb9d3cbe05d1bd2be5cc9953fd'] = 'Male';
-$_MODULE['<{byjuno}prestashop>payment_execution_b719ce180ec7bd9641fece2f920f4817'] = 'Female';
-$_MODULE['<{byjuno}prestashop>payment_execution_10803b83a68db8f7e7a33e3b41e184d0'] = 'Date of Birth';
-$_MODULE['<{byjuno}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'Other payment methods';
-$_MODULE['<{byjuno}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'I confirm my order';
+$_MODULE['<{byjuno}prestashop>payment_execution_962c29e9d818d87968b7460bf9a6472b'] = 'Selezionare il metodo di pagamento';
+$_MODULE['<{byjuno}prestashop>payment_execution_879f6b8877752685a966564d072f498f'] = 'Non ci sono articoli nel carrello.';
+$_MODULE['<{byjuno}prestashop>payment_execution_de373d72d209506f178c46735ecbf447'] = 'Selezionare il piano di pagamento';
+$_MODULE['<{byjuno}prestashop>payment_execution_eaf1849876a6548ecc5b804d7da44b27'] = 'Selezionare la consegna della fattura';
+$_MODULE['<{byjuno}prestashop>payment_execution_dfc27ef27b631b0e7114573a439c3180'] = 'Invio fattura tramite e-mail (senza spese aggiuntive) a';
+$_MODULE['<{byjuno}prestashop>payment_execution_3cd02748d4e2c00844a284b71a479ba1'] = 'Invio fattura cartacea tramite posta (spese aggiuntive di CHF 3.50) a';
+$_MODULE['<{byjuno}prestashop>payment_execution_019ec3132cdf8ee0f2e2a75cf5d3e459'] = 'sesso';
+$_MODULE['<{byjuno}prestashop>payment_execution_63889cfb9d3cbe05d1bd2be5cc9953fd'] = 'maschio';
+$_MODULE['<{byjuno}prestashop>payment_execution_b719ce180ec7bd9641fece2f920f4817'] = 'femminile';
+$_MODULE['<{byjuno}prestashop>payment_execution_10803b83a68db8f7e7a33e3b41e184d0'] = 'data di nascita';
+$_MODULE['<{byjuno}prestashop>payment_execution_569fd05bdafa1712c4f6be5b153b8418'] = 'altri metodi di pagamento';
+$_MODULE['<{byjuno}prestashop>payment_execution_46b9e3665f187c739c55983f757ccda0'] = 'confermo il mio ordine';
 
 
 return $_MODULE;
